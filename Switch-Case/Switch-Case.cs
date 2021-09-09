04fa39a68f90426fb2b4aa24457dd1fbd465767a
Bugra Oner <boletroy@gmail.com>
2021-09-10 00:23:39 +0300

    Ödev değiştirildi

--- Switch-Case/Switch-Case.cs
+++ Switch-Case/Switch-Case.cs
@@ -4,3 +4,33 @@ switch(ifade)
   case kontrol2: işlemler2; break;
   default : işlemler3; break;
 }
+
+
+
+
+ //switch (takimKodu)
+ //         {
+ //             case "gs":
+ //                 Console.WriteLine("Galatasaray");
+ //                 break;
+ //
+ //             case "fb":
+ //                 Console.WriteLine("Fenerbahçe");
+ //                 break;
+ //
+ //             case "bjk":
+ //                 Console.WriteLine("Beşiktaş");
+ //                 break;
+ //
+ //             case "ts":
+ //                 Console.WriteLine("Trabzonspor");
+ //                 break;
+ //
+ //             case "bur":
+ //                 Console.WriteLine("Bursaspor");
+ //                 break;
+ //             default:
+ //                 Console.WriteLine("Takım bulunamadı!");
+ //                 break;
+ //         }
+ //     }
