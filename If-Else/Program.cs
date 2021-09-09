a60af5b7d71327997bf71e4c60e61ca9352c39ba
Bugra Oner <boletroy@gmail.com>
2021-09-10 00:23:01 +0300

    Ödevler Eklendi

---
+++ If-Else/Program.cs
@@ -0,0 +1,18 @@
+{
+    if (char.IsUpper(ch))
+    {
+        Console.WriteLine($"An uppercase letter: {ch}");
+    }
+    else if (char.IsLower(ch))
+    {
+        Console.WriteLine($"A lowercase letter: {ch}");
+    }
+    else if (char.IsDigit(ch))
+    {
+        Console.WriteLine($"A digit: {ch}");
+    }
+    else
+    {
+        Console.WriteLine($"Not alphanumeric character: {ch}");
+    }
+}
---
+++ Switch-Case/Switch-Case.cs
@@ -0,0 +1,6 @@
+switch(ifade) 
+{
+  case kontrol1: işlemler1; break;
+  case kontrol2: işlemler2; break;
+  default : işlemler3; break;
+}
