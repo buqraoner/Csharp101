b85ba463df083e8d0f1ac027f61597d71d1813f8
Bugra Oner <boletroy@gmail.com>
2021-09-10 00:14:45 +0300

    Ödev

---
+++ Hata-Yonetimi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hata_Yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Bir sayi giriniz:");
+            int sayi = Convert.ToInt32(Console.Readline());
+            Console.WriteLine("Girdiginiz sayi :"+sayi);
+            }
+            catch(exception ex)
+            {
+                Console.WriteLine("Hata"+ex.Message.ToString());
+
+            }
+            // finally
+            // {
+            //     Console.WriteLine("İşlem Tamamlandı.");
+            // }
+            // 
+        
+        
+        }
+
+    }
+}
