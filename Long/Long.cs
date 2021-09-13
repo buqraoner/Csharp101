8e4e76b477811806a52cd811517718be569b9bb4
Bugra Oner <boletroy@gmail.com>
2021-09-13 15:23:36 +0300

    long

---
+++ Long/Long.cs
@@ -0,0 +1,68 @@
+// // Sample Input
+// 
+// 5
+// 1000000001 1000000002 1000000003 1000000004 1000000005
+// Output
+// 
+// 5000000015
+// Note:
+// 
+// The range of the 32-bit integer is .
+// When we add several integer values, the resulting sum might exceed the above range. You might need to use long int C/C++/Java to store such sums.
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+            long toplam = 0;
+            
+            foreach (long arr in ar){
+                    
+                toplam += arr;
+            }
+            return toplam;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.aVeryBigSum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
