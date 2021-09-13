25dbec38cf4fcb1f107fcd8275dbd32413daf351
Bugra Oner <boletroy@gmail.com>
2021-09-13 18:15:28 +0300

    ödev

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,7 +6,30 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+            
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+
+            int Topla(int deger1,int deger2)
+            {
+                return(deger1+deger2);
+            }
+
+            }
+            class Metotlar
+            {
+               void EkranaYazdir(string veri)
+               {
+                   Console.WriteLine(veri);
+               }
+            }
         }
-    }
+    
 }
