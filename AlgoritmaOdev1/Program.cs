70831e35115e93688ca253ba6d5bd1edc9ed24d0
Bugra Oner <boletroy@gmail.com>
2021-09-15 23:49:03 +0300

    AlgoritmaOdevı

---
+++ AlgoritmaOdev1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmaOdev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bir adet sayi giriniz : ");
+            int sayi1 = 0;
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girilecek sayi sayisi : {0} ", sayi1);
+            int sayi2 = 0;
+
+            ArrayList ciftSayilar = new ArrayList();
+
+            for(int i = 1; i <= sayi1; i++)
+            {
+               sayi2 = Convert.ToInt32(Console.ReadLine());
+
+                if (sayi2 % 2 == 0)
+                {
+                    ciftSayilar.Add(sayi2);
+                }
+
+
+            }
+            Console.WriteLine(" ");
+            foreach(var sayilar in ciftSayilar)
+            {
+
+                Console.Write(sayilar + " ");
+            }
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
