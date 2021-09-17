f505e7dd2c0ca6176cc6fdc796f6b3232484de42
Bugra Oner <boletroy@gmail.com>
2021-09-17 11:51:26 +0300

    Algoritma Ödevi ıp

---
+++ Odev1AlgoritmaSoruları/AlgoritmaOdev3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmaOdev3
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bir adet Pozitif tam sayi giriniz   : ");
+
+            int sayitutucu = Convert.ToInt32(Console.ReadLine());
+
+           
+            char[] kelimeler = new char[sayitutucu-1];
+          
+
+
+
+
+
+
+            Console.WriteLine("{0}   >>  Adet Kelimeyi tane tane giriniz ",sayitutucu);
+           
+            for (int i = 0; i <= kelimeler.Length ; i++)
+            {
+
+                Console.WriteLine("Bir kelime giriniz  : ");
+                kelimeler[i] = Convert.ToChar(Console.ReadLine()); 
+               
+            }
+
+
+            Console.WriteLine("Girdiğiniz kelimeler  ");
+
+            foreach(var kelime in kelimeler)
+            {
+                Console.WriteLine("{0} ", kelime);
+
+
+            }
+        }
+
+    }
+
+
+}
---
+++ Odev1AlgoritmaSoruları/AlgoritmaOdev3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AlgoritmaOdev3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlgoritmaOdev3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba5ac8ac-c547-4cfc-b164-595afe5d2919")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
