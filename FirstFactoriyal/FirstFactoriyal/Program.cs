4d06b262f9af3262b04ae6d21a52a624c63aa6b4
Bugra Oner <boletroy@gmail.com>
2021-09-17 11:52:01 +0300

    First Factoriyal

---
+++ FirstFactoriyal/FirstFactoriyal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstFactoriyal
+{
+class MainClass
+        {
+
+            public static int FirstFactorial(int num)
+            {
+               
+
+
+                // code goes here  
+                return num;
+
+            }
+
+            static void Main()
+            {
+                // keep this function call here
+                Console.WriteLine(FirstFactorial(Console.ReadLine()));
+            }
+
+        private static int FirstFactorial(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
