0639a651fd28fbfc90198b9a049512711574822e
Bugra Oner <boletroy@gmail.com>
2021-09-17 11:50:51 +0300

    Ödevler

---
+++ DateTime/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Datetimekullanımı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+
+
+
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+
+
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddHours(3));
+            Console.WriteLine(DateTime.Now.AddSeconds(30));
+            Console.WriteLine(DateTime.Now.AddMonths(5));
+            Console.WriteLine(DateTime.Now.AddYears(10));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(50));
+
+
+
+            // DateTime Formatlarıııııı 
+
+            Console.WriteLine(DateTime.Now.ToString("dd"));
+            Console.WriteLine(DateTime.Now.ToString("ddd"));
+            Console.WriteLine(DateTime.Now.ToString("dddd"));
+
+
+            Console.WriteLine(DateTime.Now.ToString("MM"));
+            Console.WriteLine(DateTime.Now.ToString("MMM"));
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));
+
+
+            Console.WriteLine(DateTime.Now.ToString("yy"));
+            Console.WriteLine(DateTime.Now.ToString("yyyy"));
+
+            Console.WriteLine(Math.Ceiling(22.3)); //23
+            Console.WriteLine(Math.Round(22.3)); //22
+            Console.WriteLine(Math.Round(22.7)); //23
+            Console.WriteLine(Math.Floor(22.7)); //22 
+
+        }
+    }
+}
---
+++ HazirMetodlar/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace HazirMetodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Dersimiz CSharp,Hoşgeldiniz!";
+            string degisken2 = " CSharp";
+
+            // Length;
+
+            Console.WriteLine(degisken.Length);
+
+            //TopUpper ,ToLower
+            Console.WriteLine(degisken.ToUpper());
+            Console.WriteLine(degisken.ToLower());
+
+            //Concat 
+            Console.WriteLine(String.Concat(degisken,  " Merhaba!"));
+
+
+            //Compare,CompareTo
+
+            Console.WriteLine(degisken.CompareTo(degisken2));
+            Console.WriteLine(String.Compare(degisken, degisken2, true));
+            Console.WriteLine(String.Compare(degisken, degisken2, false));
+            // false buyuk kücük harf duyarlı
+            // true duyarli
+
+            //Contains
+
+            Console.WriteLine(degisken.Contains(degisken2));
+            Console.WriteLine(degisken.EndsWith("Hoşgeldiniz!"));
+            Console.WriteLine(degisken.StartsWith("Merhaba"));
+
+
+            // IndexOf
+            Console.WriteLine(degisken.IndexOf("CS"));
+            Console.WriteLine(degisken.LastIndexOf("i"));
+            Console.WriteLine(degisken.IndexOf("Bugra"));
+            //bulamazsa -1
+            // İlk indexi verirr... 
+
+
+            //Insert
+            Console.WriteLine(degisken.Insert(0,"Merhaba! "));
+            // 0. İndexten başla Merhaba Ekle 
+
+
+            //PadLeft  // PadRight ;
+
+            Console.WriteLine(degisken+ degisken2.PadLeft(30));
+            Console.WriteLine(degisken+ degisken2.PadLeft(30,'*'));
+            Console.WriteLine(degisken.PadRight(50)+ degisken2.PadLeft(30,'*'));
+            Console.WriteLine(degisken.PadRight(50,'-')+ degisken2.PadLeft(30,'*'));
+
+            //Remove 
+            Console.WriteLine(degisken.Remove(10));
+            Console.WriteLine(degisken.Remove(5,3));
+            Console.WriteLine(degisken.Remove(0,1));
+
+            //10.İndexten başla sonuna kadar sil 
+
+            //RePlace 
+            Console.WriteLine(degisken.Replace("CSharp", "C#"));
+            Console.WriteLine(degisken.Replace(" ", "+"));
+
+
+            //Split
+
+            Console.WriteLine(degisken.Split(' ')[1]);
+
+            //Substring
+            Console.WriteLine(degisken.Substring(4));
+            Console.WriteLine(degisken.Substring(4, 6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ;
+
+
+
+        }
+    }
+}
