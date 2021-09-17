5295b8e5073f37d5806029ee0e6a1cde46e77f9b
Bugra Oner <boletroy@gmail.com>
2021-09-17 13:27:10 +0300

    asdfas

---
+++ StringInt_Mi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StringInt_Mi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+                 Console.WriteLine("Hello World!");
+
+             
+
+    
+        string S = Console.ReadLine();
+        
+        if(S is string){
+            
+            Console.WriteLine(S);
+        }
+        else 
+        {
+            Console.WriteLine("Bad String");
+        }
+
+        
+
+
+
+
+
+}
+    }
+
+}
+
+
+
