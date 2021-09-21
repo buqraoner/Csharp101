1d045a234b0e9dd993ed0aa797104be4bd0e9f2a
Bugra Oner <boletroy@gmail.com>
2021-09-21 21:05:04 +0300

    Uptaded

--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -6,7 +6,22 @@ namespace inheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+                    //      Canlılar
+                    // bitkiler             hayvanlar                
+                    //
+                    //
+                    //
+                    //
+                    //Tohumlu           Tohumsuz        sürüngenler 
+            TohumluBitkiler tohumluBitki = new tohumluBitki();
+
+            tohumluBitki.Beslenme();
+            tohumluBitki.Solunum();
+            tohumluBitki.Bosaltim();
+            tohumluBitki.FotosentezYapmak();
+            tohumluBitki.TohumlaCogalma();
+            
+
+        }            
+    }            
+}         
