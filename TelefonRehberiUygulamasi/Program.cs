f573ac4dadea5a91cdabe78ded861538d18b94df
Buğra Öner <83416454+buqraoner@users.noreply.github.com>
2021-09-30 18:13:23 +0300

    Add files via upload

---
+++ TelefonRehberiUygulamasi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelefonRehberiUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRehberLogger rehberManager = new RehberManager();
+        }
+    }
+}
