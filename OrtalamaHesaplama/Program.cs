78ce5bc063db177537f5b704a540220c38c817f0
Buğra Öner <boletroy@gmail.com>
2021-11-07 00:01:16 +0300

    Diziler

---
+++ OrtalamaHesaplama/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+    /*
+            Ortalama Hesaplama
+Kulanıcıdan alınan derinliğe göre fibonacci serisindeki rakamların ortalamasını alıp ekrana yazdıran uygulamayı yazınız.
+
+Dikkat Edilmesi Gereken Noktalar :
+Kod tekrarından kaçınılmalı
+Single Responsibility kuralına uygun şekilde, uygulama sınıflara ve metotlara bölünmeli.
+            
+    */
+
+
+namespace OrtalamaHesaplama
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Bir adet değer giriniz ");
+
+            var Deger = Console.ReadLine();
+
+
+
+
+
+           Console.WriteLine("Girdiğiniz dizinin boyutu : "+ Deger);
+
+        
+          
+              
+
+
+
+
+        
+        }
+    }
+}
