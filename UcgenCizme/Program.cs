16a426e58e2c87078067c7cb92b4d1ca248cbd86
Buğra Öner <boletroy@gmail.com>
2021-11-09 23:29:54 +0300

    ProjeÖdevi 2

---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+    /*
+    Kullanıcıdan alınan boyut bilgisine göre console'a Üçgen çizen uygulamayı yazınız.
+
+    Dikkat Edilmesi Gereken Noktalar :
+
+    Kod tekrarından kaçınılmalı
+    Single Responsibility kuralına uygun şekilde, uygulama sınıflara ve metotlara bölünmeli.
+
+*/  
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+             
+    }
+
+
+    }
+
+}
+
+                   
+
+               
+                 
+
+
+
+
+
+
+
+
+   
+              
+              
+              
+              
+                    
+                
+            
+
+            
+
+
+ 
